11c9b767bdb9d193645f0ecda9a49a21fa038702
forcus <599202537@qq.com>
2024-06-04 00:03:08 +0800

    aa

---
+++ Common/CPP/NFComm/GamePlugin/GameMapModule/GameEntity.h
@@ -0,0 +1,9 @@
+#ifndef GAME_ENTITY_H
+#define GAME_ENTITY_H
+
+class GameEntity
+{
+
+};
+
+#endif
--- Common/CPP/NFComm/GamePlugin/GameMapModule/GameMapModule.h
+++ Common/CPP/NFComm/GamePlugin/GameMapModule/GameMapModule.h
@@ -1,5 +1,19 @@
 #ifndef GAME_MAP_MODULE_H
 #define NF_COMMONCONFIG_MODULE_H
+#include "NFComm/NFLuaScriptPlugin/NFLuaRequire.h"
+
+class  GameMap
+{
+#define LUA_CLASS GameMap
+#define LUA_PS \
+
+#define LD LD1
+	LP1
+#define LD LD2
+		LP2
+#undef LD
+#undef LUA_CLASS
+};
 
 class GameMapModule
 {
